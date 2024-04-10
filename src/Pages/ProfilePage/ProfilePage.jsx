23c831a6cb612ca24422ea701505c01365976736
Leon Williams <leonbwilliams6@gmail.com>
2024-04-11 00:54:52 +0100

    Add sport and skill blocks to user profile page

--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,22 +1,22 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import "./ProfilePage.scss"
-import DatePicker from 'react-date-picker';
+import { Link, useNavigate } from "react-router-dom";
+import "./ProfilePage.scss";
+import DatePicker from "react-date-picker";
 import Button from "../../Components/Button/Button";
-import TimeRangePicker from '@wojtekmaj/react-timerange-picker';
-import 'react-date-picker/dist/DatePicker.css';
-import 'react-calendar/dist/Calendar.css';
-import '@wojtekmaj/react-timerange-picker/dist/TimeRangePicker.css';
-import 'react-clock/dist/Clock.css';
+import TimeRangePicker from "@wojtekmaj/react-timerange-picker";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
+import "@wojtekmaj/react-timerange-picker/dist/TimeRangePicker.css";
+import "react-clock/dist/Clock.css";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+import { MdModeEdit } from "react-icons/md";
 
 const ProfilePage = () => {
-
-    const [user, setUser] = useState(null);
-    const [failedAuth, setFailedAuth] = useState(false);
-    const [date, setDate] = useState(new Date());
-    const [time, setTime] = useState(['10:00', '11:00']);
-
+  const [user, setUser] = useState(null);
+  const [failedAuth, setFailedAuth] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState(["10:00", "11:00"]);
 
   useEffect(() => {
     const getProfile = async () => {
@@ -72,34 +72,45 @@ const ProfilePage = () => {
 
   return (
     <main>
-        <div className="profile">
-            <div className="profile__container">
-                <div className="profile__body">
-                <div className="profile__banner">
-                    <div className="profile__avatar">{user.first_name[0]}</div>
-                    <h2 className="profile__welcome">
-                        Welcome Back, {user.first_name} {user.last_name}!
-                    </h2>
-                    <Button className="profile__logout" onClick={handleLogout}>
-                        Log out
-                    </Button>
-                </div>
-                    <section className="profile__find-game">
-                        <div className="profile__date">
-                            <DatePicker onChange={setDate} value={date} />
-                        </div>
-                        <div className="profile__time">
-                            <TimeRangePicker onChange={setTime} value={time} />
-                        </div>
-                        <Button className="profile__game">
-                            Find a Game
-                        </Button>
-                    </section>
-                </div>
+      <div className="profile">
+        <div className="profile__container">
+          <div className="profile__body">
+            <div className="profile__banner">
+              <div className="profile__avatar">{user.first_name[0]}</div>
+              <h2 className="profile__welcome">
+                Welcome Back, {user.first_name} {user.last_name}!
+              </h2>
+              <Button className="profile__logout" onClick={handleLogout}>
+              <FaArrowRightFromBracket className="profile__icon"/>
+              </Button>
             </div>
+            <section className="profile__sport">
+            <h3 className="profile__heading">Sport: </h3>
+              <h4 className="profile__chosen-sport">{user.sport}</h4>
+              <Button className="profile__edit"><MdModeEdit /></Button>
+            </section>
+            <section className="profile__skill">
+            <h3 className="profile__heading">Skill Level: </h3>
+              <h4 className="profile__skill-level">{user.skill_level}</h4>
+              <Button className="profile__edit"><MdModeEdit /></Button>
+            </section>
+            <section className="profile__find-game">
+              <h3 className="profile__heading">Search for availability</h3>
+              <section className="profile__available">
+                <div className="profile__date">
+                  <DatePicker onChange={setDate} value={date} />
+                </div>
+                <div className="profile__time">
+                  <TimeRangePicker onChange={setTime} value={time} />
+                </div>
+              </section>
+              <Button className="profile__game">Find a Game</Button>
+            </section>
+          </div>
         </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
